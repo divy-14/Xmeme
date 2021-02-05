b6bf13cdb08e1750415dca79c27b8e02806c07a6
divy-14 <divymohanrai@gmail.com>
2021-02-06 01:54:32 +0530

    initial commiy

---
+++ postmeme/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Post(models.Model):
+    name = models.CharField(max_length=50)
+    caption = models.CharField(max_length=200)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.caption
---
+++ postmeme/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .forms import PostForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
+def home(request):
+
+    if request.method == "GET":
+        return render(request, 'postmeme/home.html', {'form': PostForm()})
+
+    else:
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Meme created successfully')
+        return HttpResponseRedirect(reverse('postmeme-home'))

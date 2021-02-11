cddc8aebce2c4518f5c69ef6916886e31e6f3313
Divy Mohan Rai <divymohanrai@gmail.com>
2021-02-12 02:35:44 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 sudo apt-get upgrade -y
 sudo apt-get update -y
 sudo apt install python3.8 -y
-sudo apt-get install python3-venv -y
-python3 -m venv env
-source ./env/bin/activate
+sudo apt-get -y install python3-pip
+sudo apt-get install curl -y
 pip3 install -r requirements.txt

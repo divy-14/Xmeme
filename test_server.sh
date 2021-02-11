67f946a6ef64930d935cd7dad3ed9848a1d97c02
Divy Mohan Rai <divymohanrai@gmail.com>
2021-02-11 17:27:03 +0530

    Add files via upload

--- test_server.sh
+++ test_server.sh
@@ -1,16 +1,11 @@
+#!/bin/bash
 chmod +x install.sh
 sudo ./install.sh
-
 chmod +x server_run.sh
 ./server_run.sh &
-
 chmod +x sleep.sh
 ./sleep.sh
-
 curl --location --request GET http://localhost:8081/memes
-
 curl --location --request POST http://localhost:8081/memes --header Content-Type:application/json --data-raw "{\"name\":\"Hello testing\",  \"url\":\"https://i.ytimg.com/vi/if-2M3K1tqk/maxresdefault.jpg\",  \"caption\":\"This is a testing meme\"}"
-
 curl --location --request GET http://localhost:8081/memes
-
 curl --location --request GET http://localhost:8081/swagger-ui
